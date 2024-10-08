6476eae1dc22a8ac23bd0482d32333c1071a72fb
Barasa-Micah <barasamica@gmail.com>
2024-10-08 17:03:27 +0300

    Created login logic

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,3 +4,25 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/Ionicons';
+
+
+const LoginScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+
+    // Login function 
+    const handleLogin = () => {
+        if (!email || !password) {
+            Alert.alert('Error', 'Please enter both email and password.')
+            return;
+        }
+
+        // Simulating a successful login
+        Alert.alert('Login', 'Login successful!' )
+    };
+    return(
+        
+    )
+}
