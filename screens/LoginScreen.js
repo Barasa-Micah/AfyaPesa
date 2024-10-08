6deb01700ad0872e8d645387b999e52664c10eae
Barasa-Micah <barasamica@gmail.com>
2024-10-08 16:54:26 +0300

    Creating the login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import {
+  View, Text, TextInput, TouchableOpacity, StyleSheet, Alert
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Ionicons';
